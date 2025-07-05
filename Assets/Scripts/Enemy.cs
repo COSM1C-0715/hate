94e27fcfaa3536cb6ddfead1bbff99fe89d868d1
a <KTC25A31A0042@edu.kyoto-tech.ac.jp>
2025-07-05 16:29:58 +0900

    答え合わせ

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -31,8 +31,7 @@ public class Enemy : MonoBehaviour
 	/// </summary>
 	public void Start()
     {
-		Quaternion rot = Quaternion.identity;
-		worldMatrix_.SetTRS(player_.transform.position - transform.position, rot, Vector3.one);
+		worldMatrix_.SetTRS(transform.position,Quaternion.identity, Vector3.one);
     }
 
     /// <summary>  
@@ -41,32 +40,26 @@ public class Enemy : MonoBehaviour
     public void Update()
     {
 		var normalZ = new Vector3(0, 0, 1);
-		var EnemyForward = player_.worldMatrix * normalZ;
-		var EnemyViewCos = Mathf.Cos(player_.viewRadian);
-		Matrix4x4 scaleMatrix = Matrix4x4.identity;
-		Matrix4x4 transMatrix = Matrix4x4.identity;
-		Vector4 trans = new Vector4();
-		Vector3 scale = new Vector3();
+		var EnemyForward =worldMatrix_ * normalZ;		
 		var EnemyToPlayer = (player_.transform.position - transform.position).normalized;
+		var EnemyViewCos = Mathf.Cos(20.0f * Mathf.Deg2Rad);
 		var dot = Vector3.Dot(EnemyForward, EnemyToPlayer);
 		if (EnemyViewCos <= dot)
 		{
-			Debug.Log("A");
-			trans.Set(0f, 0f, 0f, 0);
-			scale.Set(1f, 1f, 1f);
-			trans.z = 0.2f * Time.deltaTime;
-			var toTarget = (player_.transform.position - transform.position).normalized;
-			var foward = transform.forward;
-			if (0.999f < dot) { return; }
-			var radian = Mathf.Acos(dot);
-			var cross = Vector3.Cross(foward,toTarget);
+			var cross = Vector3.Cross(EnemyForward, EnemyToPlayer);
+			var radian = Mathf.Min(Mathf.Acos(dot),(10.0f * Mathf.Deg2Rad));
 			radian *= (cross.y / Mathf.Abs(cross.y));
-			transform.rotation *= Quaternion.Euler(0, Mathf.Rad2Deg * radian, 0);
-			scaleMatrix = scaleMatrix * Matrix4x4.Scale(scale);
-			transMatrix = transMatrix * Matrix4x4.Scale(trans);
-			Matrix4x4 matrix = (transMatrix * scaleMatrix) * worldMatrix_;
-			transform.position = matrix.GetColumn(3);
-		}
+			var rotMatrix = Matrix4x4.Rotate(Quaternion.Euler(0, Mathf.Rad2Deg * radian, 0));
+			worldMatrix_ = worldMatrix_ * rotMatrix;
 
+			var f = new Vector3(0, 0, 0.2f);
+			var move = worldMatrix_ * f;
+
+			var pos = worldMatrix_.GetColumn(3) + move;
+			worldMatrix_.SetColumn(3, pos);
+		}
+		transform.position = worldMatrix_.GetColumn(3);
+		transform.rotation = worldMatrix_.rotation;
+		transform.localScale = worldMatrix_.lossyScale;
 	}
 }
