f2c75c3257ba5bd339856d0d219b8e5d4da0b05d
GTUNE2-01\ktc <213023361+COSM1C-0715@users.noreply.github.com>
2025-07-05 14:41:24 +0900

    動いた
    動いたというよりテレポートしてる

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -31,6 +31,8 @@ public class Enemy : MonoBehaviour
 	/// </summary>
 	public void Start()
     {
+		Quaternion rot = Quaternion.identity;
+		worldMatrix_.SetTRS(player_.transform.position - transform.position, rot, Vector3.one);
     }
 
     /// <summary>  
@@ -41,11 +43,18 @@ public class Enemy : MonoBehaviour
 		var normalZ = new Vector3(0, 0, 1);
 		var EnemyForward = player_.worldMatrix * normalZ;
 		var EnemyViewCos = Mathf.Cos(player_.viewRadian);
+		Matrix4x4 scaleMatrix = Matrix4x4.identity;
+		Matrix4x4 transMatrix = Matrix4x4.identity;
+		Vector4 trans = new Vector4();
+		Vector3 scale = new Vector3();
 		var EnemyToPlayer = (player_.transform.position - transform.position).normalized;
 		var dot = Vector3.Dot(EnemyForward, EnemyToPlayer);
 		if (EnemyViewCos <= dot)
 		{
 			Debug.Log("A");
+			trans.Set(0f, 0f, 0f, 0);
+			scale.Set(1f, 1f, 1f);
+			trans.z = 0.2f * Time.deltaTime;
 			var toTarget = (player_.transform.position - transform.position).normalized;
 			var foward = transform.forward;
 			if (0.999f < dot) { return; }
@@ -53,6 +62,10 @@ public class Enemy : MonoBehaviour
 			var cross = Vector3.Cross(foward,toTarget);
 			radian *= (cross.y / Mathf.Abs(cross.y));
 			transform.rotation *= Quaternion.Euler(0, Mathf.Rad2Deg * radian, 0);
+			scaleMatrix = scaleMatrix * Matrix4x4.Scale(scale);
+			transMatrix = transMatrix * Matrix4x4.Scale(trans);
+			Matrix4x4 matrix = (transMatrix * scaleMatrix) * worldMatrix_;
+			transform.position = matrix.GetColumn(3);
 		}
 
 	}
